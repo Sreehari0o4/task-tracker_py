311af0887644f527b4ae79a71fe632e6f41f7bda
Sreehari0o4 <sreehariskumarwrk01@gmail.com>
2025-05-28 23:49:24 +0530

    Learned about process.argv

--- tracker.js
+++ tracker.js
@@ -1,4 +1,14 @@
 const args = process.argv
+/* 
+process.argv is an array that holds all command-line arguments.
+
+The first two items are always:
+The path to Node.js
+The path to your script
+
+Any arguments after that are your actual inputs.
+
+*/
 
 console.log("Full arguments:", args);
 console.log("Command:",args[2]);
